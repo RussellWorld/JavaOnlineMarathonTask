ac50fc5b389d2bed0b3b014b2ae863965bcdf6d2
Rassell <miraqq55@gmail.com>
2020-12-02 00:10:51 +0100

    Day2

---
+++ src/Day2/Task8.java
@@ -0,0 +1,21 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Task8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число ");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число ");
+        int b = scanner.nextInt();
+
+        for (int i = a + 1; i < b; i++) {
+            if (i % 5 == 0 && i % 10 != 0) {
+                System.out.print(" " + i);
+            }
+        }
+    }
+}
+
+
