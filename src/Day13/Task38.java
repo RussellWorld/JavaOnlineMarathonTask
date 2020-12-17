861bfd4ae2a54672f996032b5143d24494126150
Rassell <miraqq55@gmail.com>
2020-12-17 16:37:47 +0100

    Day13

---
+++ src/Day13/Task38.java
@@ -0,0 +1,30 @@
+package Day13;
+
+public class Task38 {
+    public static void main(String[] args) {
+
+        User user1 = new User("user1");
+        User user2 = new User("user2");
+        User user3 = new User("user3");
+
+        user1.sendMessage(user2, "Hello from user!");
+        user1.sendMessage(user2, "How are you?");
+
+        user2.sendMessage(user1, "Hi, user1!");
+        user2.sendMessage(user1, "I'm user2. Nice to meet you.");
+        user2.sendMessage(user1, "I'm fine, and you?");
+
+        user3.sendMessage(user1, "Hey! My name is user3.");
+        user3.sendMessage(user1, "Do you remember me?");
+        user3.sendMessage(user1, "We went to the same school");
+
+        user1.sendMessage(user3, "Hey, user3. Of course I remember you!");
+        user1.sendMessage(user3, "So nice to see you again");
+        user1.sendMessage(user3, "Let's meet up. Are you free tommorow?");
+
+        user3.sendMessage(user1, "Yep, I'm free tomorrow. Let's go.");
+
+        MessageDatabase.showDialog(user1, user3);
+
+    }
+}
---
+++ src/Day13/User.java
@@ -0,0 +1,49 @@
+package Day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String userName;
+    private List<User> subscriptions;
+
+    public User(String userName) {
+        this.userName = userName;
+        this.subscriptions = new ArrayList<>();
+    }
+
+    public void subscribe(User user) {
+        this.subscriptions.add(user);
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public List<User> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public boolean isSubscrubed(User user) {
+        for (User currentUser : subscriptions) {
+            if (currentUser.getUserName().equals(user.getUserName()))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean isFriend(User user) {
+        return this.isSubscrubed(user) && user.isSubscrubed(this);
+    }
+
+    public void sendMessage(User user, String text) {
+        MessageDatabase.addNewMessage(this, user, text);
+    }
+
+    public String toString() {
+        return userName;
+    }
+}
+
+
+
