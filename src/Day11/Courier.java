17ee844cf4688dfa144e0854d6d1342425d9621b
Rassell <miraqq55@gmail.com>
2020-12-17 12:40:08 +0100

    Day11

---
+++ src/Day11/Courier.java
@@ -0,0 +1,28 @@
+package Day11;
+
+public class Courier implements Worker {
+    private int salary;
+    private Warehouse warehouse;
+
+    public Courier(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    @Override
+    public void doWork() {
+        salary += 100;
+        warehouse.setBalance(warehouse.getBalance() + 1000);
+    }
+
+    @Override
+    public void bonus() {
+        if (warehouse.getBalance() > 1_00_000) salary *= 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Courier{" +
+                "salary=" + salary +
+                '}';
+    }
+}
